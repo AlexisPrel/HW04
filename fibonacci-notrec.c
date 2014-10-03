c1cc53b6857fe17affbc22ff5b1ba0faaed72ab8
Alexis Prel <alexis.prel@uconn.edu>
2014-10-03 01:58:33 -0400

    New version

--- fibonacci-notrec.c
+++ fibonacci-notrec.c
@@ -1,12 +1,19 @@
 long fibonacci(int N){
     long i, j, k;
     i = 0; /* F0 */
-    j = 1  /* F1 */
-    for(int n = 2; n < N; n++)
-    {
-        k = i +j; /* Fn  */
-        i = j; 
-        j = k;
-    }
-    return k;
+    j = 1;  /* F1 */
+    if(N == 0)
+        return 0;
+    else if(N==1)
+        return 1;
+    else
+        {
+            for(int n = 2; n <= N; n++)
+            {
+                k = i +j; /* Fn  */
+                i = j; 
+                j = k;
+            }
+            return k;
+        }
 }
--- performance-comparison.c
+++ performance-comparison.c
@@ -17,12 +17,12 @@ int main(void)
     for(i=0; i < N; i++)
     {
         l = fibonacci(i);
-        printf("%4d %201d\n",i,1);
+        printf("%4d %20ld\n",i,l);
     }
     
     end = clock();
     time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
     printf("%f sec\n", time_spent);
-    printf("%ld\n",l);   
+    
     return 0;
 }
